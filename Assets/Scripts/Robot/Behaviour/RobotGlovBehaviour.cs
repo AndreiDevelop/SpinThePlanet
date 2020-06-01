c8fd08c1613e397f6b8bcf69c37617259253c5c2
andrei_dev_wear <making714@gmail.com>
2020-06-01 23:52:31 +0300

    added outline

--- Assets/Scripts/Robot/Behaviour/RobotGlovBehaviour.cs
+++ Assets/Scripts/Robot/Behaviour/RobotGlovBehaviour.cs
@@ -1,18 +1,16 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class RobotGlovBehaviour : MonoBehaviour, IRobotBehaviour
 {
-    [SerializeField] private GameObject cub;
+    [SerializeField] private GameObject _outline;
     public void Activate(Action onActivated, params object[] param)
     {
-        cub.SetActive(true);
+        _outline.SetActive(true);
     }
 
     public void DeActivate(Action onDeActivated, params object[] param)
     {
-        cub.SetActive(false);
+        _outline.SetActive(false);
     }
 }
